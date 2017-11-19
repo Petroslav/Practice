48a2f24491c040820973deb418192f104f5edfe0
Petroslav Angelov <petroslav.angelov@gmail.com>
2017-11-19 22:59:13 +0200

    A simple line of code for practice push

--- Lesson 28/src/testing_one/TaskDemo.java
+++ Lesson 28/src/testing_one/TaskDemo.java
@@ -1,8 +1,14 @@
 package testing_one;
 
+import java.util.Scanner;
+
 public class TaskDemo {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please input a number: ");
+		int number = sc.nextInt();
+		System.out.println("Your number is: " + number);
+		System.out.println("Your number on drugs is: " + (number*0.25 + 3.14));
 	}
 }
