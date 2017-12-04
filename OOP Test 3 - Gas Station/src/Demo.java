693acb01efb29e5c8f40fd12aebca7655f171456
Petroslav Angelov <petroslav.angelov@gmail.com>
2017-12-04 21:22:34 +0200

    added the actual Demo.java file

---
+++ OOP Test 3 - Gas Station/src/Demo.java
@@ -0,0 +1,21 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		GasStation shell = new GasStation();
+		
+		for(int i = 0; i < 30; i++){
+			shell.serviceCar(new Car());
+		}
+		
+		shell.startWork();
+		shell.startGeneratingReports(true);
+		try {
+			Thread.sleep(70*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+	}
+}
